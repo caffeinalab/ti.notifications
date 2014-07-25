2e40ace5d5caa947f48a4608564e774bf7aad7a0
Flavio De Stefano <flavio.destefano@caffeinalab.com>
2014-07-25 18:57:06 +0200

    Not error but Ti.API

--- controllers/android.js
+++ controllers/android.js
@@ -26,20 +26,21 @@ $.caffeinaToastView.addEventListener('touchstart', function(){
 Initialization
 */
 
-if (!args.view) {
-	throw new Error("com.caffeinalab.titanium.notifications: In Android you MUST set a view that contain the sliding view");
+if (args.view) {
+
+	$.caffeinaToastLabel.text = args.message;
+	if (args.icon) $.caffeinaToastIcon.image = args.icon;
+
+	args.view.add($.caffeinaToastView);
+
+	$.caffeinaToastView.animate({
+		top: 0,
+		duration: args.animationDuration
+	});
+
+	// Set the timer to automatically close the Window
+	timeout = setTimeout(close, args.duration);
+
+} else {
+	Ti.API.error("com.caffeinalab.titanium.notifications: In Android you MUST set a view that contain the sliding view");
 }
-
-$.caffeinaToastLabel.text = args.message;
-if (args.icon) $.caffeinaToastIcon.image = args.icon;
-
-args.view.add($.caffeinaToastView);
-
-$.caffeinaToastView.animate({
-	top: 0,
-	duration: args.animationDuration
-});
-
-
-// Set the timer to automatically close the Window
-timeout = setTimeout(close, args.duration);
