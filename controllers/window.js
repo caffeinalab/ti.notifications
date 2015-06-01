50c53334c352f277f59a70ade7bde6b03bde30f6
adamkearsley <djkearsley@hotmail.com>
2015-06-01 15:42:05 +0100

    Added info and warn options
    info = blue background
    warn = amber background.

--- controllers/window.js
+++ controllers/window.js
@@ -42,6 +42,14 @@ exports.setStyle = function(style) {
 	   		Ti.API.info('ERROR STYLE');
 	    	$.caffeinaToastView.backgroundColor = '#80FF0000';
 	        break;
+	    case 'info':
+	   		Ti.API.info('INFO STYLE');
+	    	$.caffeinaToastView.backgroundColor = '#8000c0ff';
+	    	break;
+    	case 'warn':
+	   		Ti.API.info('WARN STYLE');
+	    	$.caffeinaToastView.backgroundColor = '#80ffc000';
+	    	break;
       };
 };
 
